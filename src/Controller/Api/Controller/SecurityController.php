a1db02dca88388c39e7e2cf295ee6c1b62d5a90d
Benoît Cyrille ONDOUA NGAZOA <benoit.ondoua@nova-technology.fr>
2023-09-22 22:16:46 +0200

    correction des buggs

--- src/Controller/Api/Controller/SecurityController.php
+++ src/Controller/Api/Controller/SecurityController.php
@@ -5,17 +5,37 @@ namespace App\Controller\Api\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class SecurityController extends AbstractController
 {
+    private $authorizationChecker;
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
     public function login()
     {
         $user = $this->getUser();
 
-        if (null === $user) {
+        if (null === $user ) {
             return $this->json([
                 'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($this->authorizationChecker->isGranted('ROLE_STUDENT')) {
+            return $this->json([
+                'message' => 'Accès autorisé uniquement aux apprenants',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ( !in_array('ROLE_STUDENT', $user->getRoles())) {
+            return $this->json([
+                'message' => 'Accès autorisé uniquement aux apprenants',
             ], Response::HTTP_UNAUTHORIZED);
         }
 
